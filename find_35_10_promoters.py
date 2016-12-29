00fe46ea662dd5b285d6d455f2ed426961d0fe6f
Auguste Dutcher <Aug@Augustes-MBP.att.net>
2016-12-28 18:03:43 -0600

    fundamental restructuring and re-definition of extended promoter (TG*TATAAT)

--- find_35_10_promoters.py
+++ find_35_10_promoters.py
@@ -4,16 +4,17 @@ from Bio.Alphabet import IUPAC
 from Bio.SeqUtils import nt_search
 import csv
 
-gb_path = 'NC_004350_2.gb'
-ref_record = SeqIO.read(open(gb_path), 'genbank')
-ref_sequence = ref_record.seq
-#ref_sequence = Seq('CCCCCCCCCCTGGGGGGGGGGGGGCATTATACCCCCCCCCCTTTTTTTTTTTGGTCAATTTTTTAAAAAAAAAACCCCCCCCCCGGGGGGGGGGTTTTTTTTTTAAAAAAAAAACCCCCCCCCCGGGGGGGGGGTTTTTTTTTTAAAAAAAAAA', IUPAC.unambiguous_dna)
+#gb_path = 'NC_004350_2.gb'
+#ref_record = SeqIO.read(open(gb_path), 'genbank')
+#ref_sequence = ref_record.seq
+ref_sequence = Seq('CCCTTGACACCCCCCCCCCCCCCCCCCTATAATCCCCCCCCCCGGGGGGGGGGCCCCCCCCCCGGGGGGGGGGGGGGGG', IUPAC.unambiguous_dna)
+print(ref_sequence)
 print(len(ref_sequence))
 
 # List of transcription start sites. 
 ### Csv with [0] uid, [1] threshold, [2] genome, [3] feature name, [4] strand, [5] TSS
 TSS_list = []
-with open('random_input.csv', 'r') as TSS_input:
+with open('sample_tss_list.csv', 'r') as TSS_input:
     TSS_reader = csv.reader(TSS_input, delimiter = ',')
     next(TSS_reader, None) # ignore the header row
     for row in TSS_reader:
@@ -30,16 +31,87 @@ thirtyfive_promoters = ['TTGANN', 'TTGNCN', 'TTGNNA', 'TTNACN', 'TTNANA', 'TTNNC
 ten_upstream_margin = 50
 ten_downstream_margin = 10
 
-def search_for_substring(promoter_list, seq_to_search):
-    search_array = []
+# Where to search for -35 promoters relative to -10 promoters
+min_len_gap = 15
+max_len_gap = 20
+
+def search_for_promoters(promoter_list, promoter_type, TSS, window_start, window_end, ref_seq, strand):
+    unique_search_results = set()
+    if strand == 'F':
+        seq_to_search = ref_seq[window_start:window_end]
+    elif strand == 'R':
+        seq_to_search = ref_seq[window_start:window_end].reverse_complement()
+    else: print('strand error')
     for promoter in promoter_list:
         search_results = nt_search(str(seq_to_search), promoter)
-        search_array.append(search_results)
-    return search_array
+        num_promoters_found = len(search_results) - 1
+        if num_promoters_found > 0:
+            for pos in search_results[1:]:
+                if strand == 'F':
+                    promoter_pos = window_start + pos
+                    distance_to_TSS = promoter_pos + 1 - TSS
+                    if promoter_type == -10:
+                        promoter_seq = str(ref_seq[(promoter_pos - 3):(promoter_pos + 6)])
+                    elif promoter_type == -35:
+                        promoter_seq = str(ref_seq[promoter_pos:(promoter_pos + 6)])
+                elif strand == 'R':
+                    promoter_pos = window_end - pos
+                    distance_to_TSS = TSS - promoter_pos
+                    if promoter_type == -10:
+                        promoter_seq = str(ref_seq[(promoter_pos - 7):(promoter_pos + 3)].reverse_complement())
+                    elif promoter_type == -35:
+                        promoter_seq = str(ref_seq[(promoter_pos - 6):promoter_pos].reverse_complement())
+                else: print('strand error')
+                unique_search_results.add((promoter_pos, distance_to_TSS, promoter_seq))
+    return unique_search_results
 
-with open('random_search_results.csv', 'w') as outfile:
+def record_thirtyfive_ten_promoters(ref_seq, TSS, strand, upstream_margin, downstream_margin, min_len_gap, max_len_gap):
+    full_results = []
+    if strand == 'F':
+        # Subtract one from TSS to convert to pythonic indexing.
+            ### Not necessary for end position because pythonic range is 'up to but not including'
+            # Define the window to look for -10 promoters
+        ten_window_start_pos = TSS - 1 - ten_upstream_margin
+        ten_window_end_pos = TSS + ten_downstream_margin
+        max_search_win = ref_seq[(TSS - 1 - (upstream_margin + max_len_gap + 6)):(TSS + downstream_margin)]
+    elif strand == 'R':
+        ten_window_start_pos = TSS - 1 - ten_downstream_margin
+        ten_window_end_pos = TSS + ten_upstream_margin
+        max_search_win = ref_seq[(TSS - 1 - downstream_margin):(TSS + (upstream_margin + max_len_gap + 6))].reverse_complement()
+    else: print('strand error')
+    ten_promoter_results = search_for_promoters(ten_promoters, -10, TSS, ten_window_start_pos, ten_window_end_pos, ref_sequence, strand)
+    print(ten_promoter_results)
+    exists_thirtyfive_promoter_results = False
+    if len(ten_promoter_results) > 0:
+        for promoter_record in ten_promoter_results:
+            ten_promoter_pos = promoter_record[0]
+            ten_promoter_distance_to_TSS = promoter_record[1]
+            ten_promoter_seq = promoter_record[2]
+            if strand == 'F':
+                thirtyfive_win_start = ten_promoter_pos - (max_len_gap + 6)
+                thirtyfive_win_end = ten_promoter_pos - min_len_gap
+            elif strand == 'R':
+                thirtyfive_win_start = ten_promoter_pos + min_len_gap
+                thirtyfive_win_end = the_promoter_pos + (max_len_gap + 6)
+            else: print('strand error')
+            thirtyfive_promoter_results = search_for_promoters(thirtyfive_promoters, -35, TSS, thirtyfive_win_start, thirtyfive_win_end, ref_sequence, strand)
+            if len(thirtyfive_promoter_results) > 0:
+                # -35 promoters boolean switched to true if any of the -10 promoters have -35 promoters
+                exists_thirtyfive_promoter_results = True
+                if ten_promoter_seq.startswith('TG'):
+                    full_results.append((str(max_search_win), ten_promoter_pos, ten_promoter_distance_to_TSS, ten_promoter_seq, 'extended', thirtyfive_promoter_results))
+                else:
+                    full_results.append((str(max_search_win), ten_promoter_pos, ten_promoter_distance_to_TSS, ten_promoter_seq, ' ', thirtyfive_promoter_results))
+            else:
+                if ten_promoter_seq.startswith('TG'):
+                    full_results.append((str(max_search_win), ten_promoter_pos, ten_promoter_distance_to_TSS, ten_promoter_seq, 'extended'))
+                else:
+                    full_results.append((str(max_search_win), ten_promoter_pos, ten_promoter_distance_to_TSS, ten_promoter_seq, ' '))
+    return (full_results, exists_thirtyfive_promoter_results)
+
+with open('test.csv', 'w') as outfile:
     writer = csv.writer(outfile, delimiter = '\t')
-    writer.writerow(['uid', 'threshold', 'genome', 'feature name', 'strand', 'TSS', 'sequence searched', 'promoter pos relative to TSS, -10 promoter seq', 'promoter pos relative to TSS, -35 promoter seq'])
+    writer.writerow(['uid', 'threshold', 'genome', 'feature name', 'strand', 'TSS', 'search results'])
     for entry in TSS_list:
         uid = entry[0]
         threshold = entry[1]
@@ -47,115 +119,11 @@ with open('random_search_results.csv', 'w') as outfile:
         feature_name = entry[3]
         strand = entry[4]
         TSS = entry[5]
-        if strand == 'F':
-            # Subtract one from TSS to convert to pythonic indexing.
-            ### Not necessary for end position because pythonic range is 'up to but not including'
-            # Define the window to look for -10 promoters
-            ten_window_start_pos = TSS - 1 - ten_upstream_margin
-            ten_window_end_pos = TSS + ten_downstream_margin
-            ten_window_to_search = ref_sequence[ten_window_start_pos:ten_window_end_pos]
-            print('feature name: ', feature_name)
-            print('length of search window: ', len(ten_window_to_search))
-            # Look for -10 promoter
-            ten_promoter_results = search_for_substring(ten_promoters, ten_window_to_search)
-            print(ten_promoter_results)
-            ten_results_array = []
-            for ten_pos_list in ten_promoter_results:
-                num_ten_results = len(ten_pos_list) - 1
-                if num_ten_results >= 1:
-                    for i, ten_entry in enumerate(ten_pos_list, start=1):
-                        if i < len(ten_pos_list):
-                            # the promoter position in pythonic index
-                            ten_promoter_pos = ten_window_start_pos + ten_pos_list[i]
-                            ten_distance_to_TSS = ten_promoter_pos + 1 - TSS
-                            ten_promoter_seq = ref_sequence[ten_promoter_pos:ten_promoter_pos + 7]
-                            # Mark if -10 'extended' promoter
-                            if ten_promoter_seq[-1] == 'G':
-                                ten_results_array.append((ten_distance_to_TSS, str(ten_promoter_seq), 'extended'))
-                            else:
-                                ten_results_array.append((ten_distance_to_TSS, str(ten_promoter_seq), ' '))
-                            # Define the window to look for -35 promoters: -15 to -20 bp from -10 promoter 
-                            thirtyfive_win_start = ten_promoter_pos - 26
-                            thirtyfive_win_end = ten_promoter_pos - 15
-                            thirtyfive_win_to_search = ref_sequence[thirtyfive_win_start:thirtyfive_win_end]
-                            print('length of search window: ', len(thirtyfive_win_to_search))
-                            print(thirtyfive_win_to_search)
-                            # Look for the -35 promoter 
-                            thirtyfive_promoter_results = search_for_substring(thirtyfive_promoters, thirtyfive_win_to_search)
-                            thirtyfive_results_array = []
-                            for thirtyfive_pos_list in thirtyfive_promoter_results:
-                                num_thirtyfive_results = len(thirtyfive_pos_list) - 1
-                                if num_thirtyfive_results >= 1:
-                                    for j, thirtyfive_entry in enumerate(thirtyfive_pos_list, start=1):
-                                        if j < len(thirtyfive_pos_list):
-                                            thirtyfive_promoter_pos = thirtyfive_win_start + thirtyfive_pos_list[j]
-                                            thirtyfive_distance_to_TSS = thirtyfive_promoter_pos + 1 - TSS
-                                            thirtyfive_promoter_seq = ref_sequence[thirtyfive_promoter_pos:thirtyfive_promoter_pos + 6]
-                                            thirtyfive_results_array.append((thirtyfive_distance_to_TSS, str(thirtyfive_promoter_seq)))
-            max_search_win = ref_sequence[(TSS - 1 - 76):(TSS + 10)]
-            print('full_search_window: ', str(max_search_win))
-            print('length_total_search_window: ', len(max_search_win))
-            # Print to results if there are any -35 promoters (which were only searched for if there were -10 promoters)
-            if len(ten_results_array) > 0:
-                unique_ten_results = set(ten_results_array)
-                if len(thirtyfive_results_array) > 0:
-                    unique_thirtyfive_results = set(thirtyfive_results_array)
-                    writer.writerow([uid, threshold, genome, feature_name, strand, TSS, str(max_search_win), unique_ten_results, unique_thirtyfive_results])
-                elif any('extended' in results[2] for results in unique_ten_results):
-                    writer.writerow([uid, threshold, genome, feature_name, strand, TSS, str(max_search_win), unique_ten_results])
-        elif strand == 'R':
-            # Subtract one from TSS to convert to pythonic indexing.
-            ### Not necessary for end position because pythonic range is 'up to but not including'
-            ten_window_start_pos = TSS - 1 - ten_downstream_margin
-            ten_window_end_pos = TSS + ten_upstream_margin
-            ten_window_to_search = ref_sequence[ten_window_start_pos:ten_window_end_pos].reverse_complement()
-            print(ten_window_to_search)
-            print('feature name: ', feature_name)
-            print('length of search window: ', len(ten_window_to_search))
-            ten_promoter_results = search_for_substring(ten_promoters, ten_window_to_search)
-            print(ten_promoter_results)
-            ten_results_array = []
-            for ten_pos_list in ten_promoter_results:
-                num_ten_results = len(ten_pos_list) - 1
-                if num_ten_results >= 1:
-                    for i, ten_entry in enumerate(ten_pos_list, start=1):
-                        if i < len(ten_pos_list):
-                            # the promoter position via pythonic counting
-                            ten_promoter_pos = ten_window_end_pos - ten_pos_list[i]
-                            ten_distance_to_TSS = TSS - ten_promoter_pos
-                            ten_promoter_seq = ref_sequence[(ten_promoter_pos - 7):ten_promoter_pos].reverse_complement()
-                            # Mark if -10 'extended' promoter
-                            if ten_promoter_seq[-1] == 'G':
-                                ten_results_array.append((ten_distance_to_TSS, str(ten_promoter_seq), 'extended'))
-                            else:
-                                ten_results_array.append((ten_distance_to_TSS, str(ten_promoter_seq), ' '))
-                            # Define the window to look for -35 promoters: -15 to -20 bp from -10 promoter 
-                            thirtyfive_win_start = ten_promoter_pos + 15
-                            thirtyfive_win_end = ten_promoter_pos + 26
-                            thirtyfive_win_to_search = ref_sequence[thirtyfive_win_start:thirtyfive_win_end].reverse_complement()
-                            print('length of search window: ', len(thirtyfive_win_to_search))
-                            print('thirtyfive search window: ', thirtyfive_win_to_search.reverse_complement())
-                            # Look for the -35 promoter 
-                            thirtyfive_promoter_results = search_for_substring(thirtyfive_promoters, thirtyfive_win_to_search)
-                            print(thirtyfive_promoter_results)
-                            thirtyfive_results_array = []
-                            for thirtyfive_pos_list in thirtyfive_promoter_results:
-                                num_thirtyfive_results = len(thirtyfive_pos_list) - 1
-                                if num_thirtyfive_results >= 1:
-                                    for j, thirtyfive_entry in enumerate(thirtyfive_pos_list, start=1):
-                                        if j < len(thirtyfive_pos_list):
-                                            thirtyfive_promoter_pos = thirtyfive_win_end - thirtyfive_pos_list[j]
-                                            thirtyfive_distance_to_TSS = TSS - thirtyfive_promoter_pos
-                                            thirtyfive_promoter_seq = ref_sequence[(thirtyfive_promoter_pos - 6):thirtyfive_promoter_pos].reverse_complement()
-                                            thirtyfive_results_array.append((thirtyfive_distance_to_TSS, str(thirtyfive_promoter_seq)))
-            max_search_win = ref_sequence[(TSS - 1 - 10):(TSS + 76)]
-            print('max_search_win: ', max_search_win.reverse_complement())
-            print('length_total_search_window: ', len(max_search_win))
-            if len(ten_results_array) > 0:
-                unique_ten_results = set(ten_results_array)
-                if len(thirtyfive_results_array) > 0:
-                    unique_thirtyfive_results = set(thirtyfive_results_array)
-                    writer.writerow([uid, threshold, genome, feature_name, strand, TSS, str(max_search_win.reverse_complement()), unique_ten_results, unique_thirtyfive_results])
-                elif any('extended' in results[2] for results in unique_ten_results):
-                    writer.writerow([uid, threshold, genome, feature_name, strand, TSS, str(max_search_win.reverse_complement()), unique_ten_results])
-
+        full_results = record_thirtyfive_ten_promoters(ref_sequence, TSS, strand, ten_upstream_margin, ten_downstream_margin, min_len_gap, max_len_gap)
+        promoter_data = full_results[0]
+        thirtyfive_promoter_presense = full_results[1]
+        if len(promoter_data) > 0:
+                # Print to results if there are any -35 promoters (which were only searched for if there were -10 promoters)
+                ### or if -10 promoter is marked 'extended'
+                if thirtyfive_promoter_presense is True or any('extended' in result[3] for result in promoter_data):
+                    writer.writerow([uid, threshold, genome, feature_name, strand, TSS, promoter_data])
